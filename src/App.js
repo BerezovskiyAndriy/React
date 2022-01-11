c2fae97865e7e7fa0ac0e779ef95626e725fcc2d
AndriyBerezovskiy <andriyxox@gmail.com>
2022-01-11 13:31:07 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import Users from "./components/Users/Users";
-import Posts from "./components/Posts/Posts";
-import Comments from "./components/Comments/Comments";
 
 function App() {
   return (
     <div className="App">
-        <Users/>
-        <Posts/>
-        <Comments/>
+
     </div>
   );
 }
