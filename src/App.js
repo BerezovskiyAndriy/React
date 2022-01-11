bd0483eaa1d124ef19e0999738e1c188065de99d
AndriyBerezovskiy <andriyxox@gmail.com>
2022-01-11 19:00:21 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SpaceX from "./components/SpaceX";
 
 function App() {
   return (
     <div className="App">
-
+        <SpaceX/>
     </div>
   );
 }
---
+++ src/components/SpaceX.js
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import SpaceXComponent from "./SpaceXComponent";
+
+const SpaceX = () => {
+    const [SpaceX,setSpaceX] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(response => response.json())
+            .then(value => setSpaceX(value))
+    },[])
+    return (
+        <div className={'wrap'}>
+            {
+                SpaceX.map(value =>
+                    value.launch_year < 2020 ?
+                        <SpaceXComponent
+                            mission_name={value.mission_name}
+                            launch_year={value.launch_year}
+                            mission_patch_small={value.links.mission_patch_small}
+                        /> : '')
+                 }
+            }
+        </div>
+    );
+};
+
+export default SpaceX;
